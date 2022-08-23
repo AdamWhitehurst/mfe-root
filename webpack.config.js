5085519287de7c895f752f3d5030ac1e2fa79e5b
Adam Whitehurst <adam.whitehurst@live.com>
2022-08-23 14:23:51 -0700

    fix(env): headers

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      allowedHosts: "all",
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
