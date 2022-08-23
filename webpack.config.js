9721e82670e384a0d4b07652261302361a98f06b
Adam Whitehurst <adam.whitehurst@live.com>
2022-08-23 12:55:39 -0700

    itty bitty innitty committy

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const { DefinePlugin } = require("webpack");
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "vasion";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new DefinePlugin({ "process.env.NODE_ENV": '"development"' }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
